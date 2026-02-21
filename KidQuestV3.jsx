e17709074340ef8f948ea8a273990f36f38a5d8a
miguelonsio7 <miguelonsio.hernandez@gmail.com>
2026-02-21 15:24:26 +0100

    Update KidQuestV3.jsx

--- KidQuestV3.jsx
+++ KidQuestV3.jsx
@@ -18,7 +18,7 @@ import { useState, useEffect, useCallback, useRef } from "react";
 
 const FIREBASE_CONFIG = {
   apiKey:            "AIzaSyBIsFxke5iu2xCSkwUw47XmfZ-B4upCr2Y",
-  databaseURL:       " https://kidquest---chore-game-default-rtdb.firebaseio.com"
+  databaseURL:       "https://kidquest---chore-game-default-rtdb.firebaseio.com",
   projectId:         "kidquest---chore-game",
 };
 
