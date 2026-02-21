40b21b93179d0d4ce6645dd4f3dcad640644415c
miguelonsio7 <miguelonsio.hernandez@gmail.com>
2026-02-21 15:50:44 +0100

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import KidQuestApp from './KidQuestV3.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <KidQuestApp />
+  </React.StrictMode>
+)
